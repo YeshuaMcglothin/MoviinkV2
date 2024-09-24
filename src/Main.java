dcb3147f18cb916de737271e5d1e2e7ee34dd23d
YeshuaMcglothin <ymcglothin1@gmail.com>
2024-09-24 14:00:31 -0700

    SafePoint

---
+++ src/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+
+//System.out.println("Hello User");
+
+        String MovieTitle = "Home Alone";
+        int MovieReleaseYear = 1990;
+        float MovieRating = 7.7f;
+        boolean IsTheMovieAFavorite = true;
+        int NumberOfReviews = 60;
+        String DirectorsName = "Dan Mazer";
+
+        String Firstname = "Jeff";
+        String Lastname = "Bezo";
+        String State = "Arizona";
+        String City = "Scottsdale";
+
+        long CardNumber = 4339238823239929L;
+        int Zipcode = 85051;
+        boolean Countryallowed = true;
+
+
+
+        System.out.println("Hello " + Firstname + " " + Lastname);
+        System.out.println("This movie is playing from " + City + ", " + State + ", " + Zipcode);
+        //System.out.println("This movie is playing from " + State);
+        System.out.println("Movie that will be playing is " + MovieTitle);
+        System.out.println("By " + DirectorsName);
+        System.out.println("Released in " + MovieReleaseYear);
+        System.out.println("Rating of " + MovieRating);
+        System.out.println("Number of reviews " + NumberOfReviews);
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
